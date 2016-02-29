2869b5f917eec51d3e549f122c5003c3a5ac41d7
admin <admin@appledemacbook-pro.local>
2016-02-29 17:51:55 +0800

    added one line

--- first/ViewController.m
+++ first/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    printf("this is a test");
 }
 
 - (void)didReceiveMemoryWarning {
